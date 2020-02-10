a264fae6a88ebc4901d6d95e527d5e17db49965e
Monutor <sporte2222@yandex.ru>
2020-02-10 20:23:21 +0300

    added products section

--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,8 @@ return gulp.src('app/scss/**/*.scss')
 gulp.task('script', function(){
   return gulp.src([
     'node_modules/slick-carousel/slick/slick.js',
-    'node_modules/magnific-popup/dist/jquery.magnific-popup.js'
+    'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
+    'node_modules/mixitup/dist/mixitup.js'
   ])
     .pipe(concat('libs.min.js'))
     .pipe(iglify())
@@ -33,7 +34,7 @@ gulp.task('style', function(){
   return gulp.src([
     'node_modules/normalize.css/normalize.css',
     'node_modules/slick-carousel/slick/slick.css',
-    'node_modules/magnific-popup/dist/jquery.magnific-popup.js'
+    'node_modules/magnific-popup/dist/magnific-popup.css'
   ])
     .pipe(concat('libs.min.css'))
     .pipe(cssmin())
